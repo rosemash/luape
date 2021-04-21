1382d13c7bea131673cfcb47bba11863b657f633
Anon Y. Mouse <anonymous@localhost>
2021-04-20 18:47:39 -0700

    Updated README to point to release 1.1.2

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -v
-i686-w64-mingw32-gcc -s -ffunction-sections -fdata-sections -Xlinker --gc-sections -Iinclude main.c include/*.c && python2 python/hack.py
+i686-w64-mingw32-gcc -ffunction-sections -fdata-sections -Xlinker --gc-sections -s -Iinclude main.c include/*.c && python2 python/hack.py
