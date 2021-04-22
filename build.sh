d032d6b59888e2b4fe1125c2144d37bee46c0220
Anon Y. Mouse <anonymous@localhost>
2021-04-22 02:14:28 -0700

    build.sh now creates 'bin' directory

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ LUASOCKET_C="deps/luasocket/*.c -D LUASOCKET_C";
 if [[ "$*" == "nosocket" ]]; then
 	LUASOCKET_C="";
 fi
+mkdir -p bin
 i686-w64-mingw32-gcc -v -ffunction-sections -fdata-sections -Xlinker --gc-sections -Os -s -Ideps/lua-5.1.5 -Ideps/luasocket main.c deps/lua-5.1.5/*.c $LUASOCKET_C -lws2_32 && python2 python/hack.py
