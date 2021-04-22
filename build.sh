0317d7169a7e14fb8ec3ba726eee6ae336eaf4e9
Anon Y. Mouse <anonymous@localhost>
2021-04-21 23:03:26 -0700

    Reverted accidental change to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-LUASOCKET_C="deps/luasocket/*.c -D LUASOCKET_C"; if [[ "$*" == "nosocket" ]]; then LUASOCKET_C=""; fi
+LUASOCKET_C="deps/luasocket/*.c -D LUASOCKET_C";
+if [[ "$*" == "nosocket" ]]; then
+	LUASOCKET_C="";
+fi
 i686-w64-mingw32-gcc -v -ffunction-sections -fdata-sections -Xlinker --gc-sections -Os -s -Ideps/lua-5.1.5 -Ideps/luasocket main.c deps/lua-5.1.5/*.c $LUASOCKET_C -lws2_32 && python2 python/hack.py
